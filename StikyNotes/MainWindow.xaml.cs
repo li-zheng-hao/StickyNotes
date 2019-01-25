168c903752078ed85dc71313f82c3404452b172f
LiZhengHao <1263212577@qq.com>
2019-01-26 00:08:10 +0800

    第一次上传，添加了一个按钮以及基本的布局

--- StikyNotes/MainWindow.xaml.cs
+++ StikyNotes/MainWindow.xaml.cs
@@ -12,17 +12,21 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace StikyNotes
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.WindowStyle = WindowStyle.None;
         }
+
+       
     }
 }
