e75838e3a5ddac1f2def24f81c909f90c4b4de57
lizhenghao <1263212577@qq.com>
2022-01-25 23:04:16 +0800

    多语言切换修复

--- StickyNotes/App.xaml.cs
+++ StickyNotes/App.xaml.cs
@@ -73,6 +73,7 @@ namespace StickyNotes
                 ProgramData.Instance.ShowAllHotKey = programData.ShowAllHotKey;
                 ProgramData.Instance.IsAutoCheckUpdate = programData.IsAutoCheckUpdate;
                 ProgramData.Instance.HideWindowData= programData.HideWindowData;
+                ProgramData.Instance.Language = programData.Language;
                 ThemeAssist.ChangeTheme(programData.CurrentTheme);
                 //有创建过的窗口
                 if (windowsDatas.Count > 0)
