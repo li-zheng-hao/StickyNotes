788775c26b39ea1f870763cb5d7938e6061ede53
LiZhengHao <1263212577@qq.com>
2022-07-13 14:37:30 +0800

    修复删除线无效的问题

--- StickyNotes/View/MainWindow.xaml.cs
+++ StickyNotes/View/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using StickyNotes.Utils;
 using StickyNotes.Utils.HotKeyUtil;
 using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 
 namespace StickyNotes
@@ -30,7 +32,26 @@ namespace StickyNotes
 
        
         }
+        // 切换删除线
+        private void StrikeCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            TextRange textRange = new TextRange((e.Source as RichTextBox).Selection.Start, (e.Source as RichTextBox).Selection.End);
+            var currentTextDecoration = textRange.GetPropertyValue(Inline.TextDecorationsProperty);
+            TextDecorationCollection newTextDecoration;
 
-       
+            if (currentTextDecoration != DependencyProperty.UnsetValue)
+                newTextDecoration = ((TextDecorationCollection)currentTextDecoration == TextDecorations.Strikethrough) ? new TextDecorationCollection() : TextDecorations.Strikethrough;
+            else
+                newTextDecoration = TextDecorations.Strikethrough;
+            textRange.ApplyPropertyValue(Inline.TextDecorationsProperty, newTextDecoration);
+
+            e.Handled = true;
+        }
+
+        private void StrikeCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
     }
 }
